612d4d20fd5dd7e1f3fe71dd5e6ee3e70394f2a3
isaac.bandeira <isaac.bandeira@gmail.com>
2022-07-11 18:10:38 -0400

    validacao de qtd de imagens por paciente

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,42 @@
+import { AppError } from 'AppError';
+// import { Request } from 'express-serve-static-core';
 import fs from 'fs';
+// import mime from 'mime-types';
+import { imagensPaciente } from 'model/ImagensPaciente.model';
+import mongoose from 'mongoose';
 import multer from 'multer';
 
 class Upload {
   private url = './images';
   private userid: any;
+  private qtdImgDB;
 
   private storage(): multer.StorageEngine {
     return multer.diskStorage({
-      destination: (request, file, callback) => {
+      destination: async (request, file, callback) => {
         this.userid = request.params.id;
-        if (!fs.existsSync(this.url)) {
-          fs.mkdirSync(this.url);
+        // valida máximo de 5 imagens por paciente
+        const qtdImagemSalva = await imagensPaciente.find({
+          paciente: new mongoose.Types.ObjectId(this.userid.toString()),
+        });
+        const qtdImgReq = request.files.length;
+        // let qtdImgDB = 0;
+        if (qtdImagemSalva !== null) {
+          this.qtdImgDB = qtdImagemSalva.length;
         }
-        callback(null, this.url);
+        const max = qtdImgReq + this.qtdImgDB;
+        if (max > 5) {
+          callback(
+            new AppError(
+              '5 (cinco) é quantidade máxima de Imagem por paciente.',
+            ),
+          );
+        } else {
+          if (!fs.existsSync(this.url)) {
+            fs.mkdirSync(this.url);
+          }
+          callback(null, this.url);
+        }
       },
       filename: (request, file, callback) => {
         callback(null, `${this.userid}-${Date.now()}-${file.originalname}`);
@@ -20,9 +44,30 @@ class Upload {
     });
   }
 
+  // private fileFilter(req, file, callback) {
+  // private fileFilter() {
+  //   return (
+  //     request: Request,
+  //     file: Express.Multer.File,
+  //     callback: multer.FileFilterCallback,
+  //   ) => {
+  //     const type = mime.extension(file.mimetype);
+  //     const fileExtensions: string[] = ['png', 'jpg', 'jpeg'];
+  //     if (fileExtensions.includes(`${type}`)) {
+  //       callback(null, true);
+  //     }
+
+  //     callback(null, false);
+  //   };
+  // }
+
   get getConfig(): multer.Options {
     return {
       storage: this.storage(),
+      limits: {
+        fileSize: 7340032,
+        parts: 5,
+      },
     };
   }
 }
--- src/route/paciente.route.ts
+++ src/route/paciente.route.ts
@@ -15,20 +15,26 @@ pacientesRoutes.get('/listid/:id', pacienteController.listById);
 pacientesRoutes.delete('/delete/:id', pacienteController.delete);
 pacientesRoutes.put('/update/:id', pacienteController.update);
 
-// upload e load de Imagem de paciente
+// upload Imagem de paciente
 pacientesRoutes.post(
   '/upload/:id',
-  multer(upload.getConfig).array('arquivos', 3),
+  multer(upload.getConfig).array('arquivos', 5),
   pacienteController.uploadImagem,
 );
+// load Imagem de paciente
 pacientesRoutes.get('/load/:id', pacienteController.loadImagem);
+// delete Imagem de paciente
+pacientesRoutes.delete('/deleteimage/:id', pacienteController.delete);
 
-// upload e load de termo de paciente
+// upload termo de paciente
 pacientesRoutes.post(
   '/uploadtermo/:id',
-  multer(upload.getConfig).array('termo', 3),
+  multer(upload.getConfig).array('termo', 5),
   pacienteController.uploadTermo,
 );
+// load termo de paciente
 pacientesRoutes.get('/loadtermo/:id', pacienteController.loadTermo);
+// delete termo de paciente
+pacientesRoutes.delete('/deletetermo/:id', pacienteController.delete);
 
 export { pacientesRoutes };
