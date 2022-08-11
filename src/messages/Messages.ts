bb42e95b05f24efd3a895ce0c893bd304a856a1c
isaac.bandeira <isaac.bandeira@gmail.com>
2022-08-11 09:20:26 -0400

    correcao delete unidade response

--- src/messages/Messages.ts
+++ src/messages/Messages.ts
@@ -7,6 +7,7 @@ export enum Messages {
   NO_UNIDADE_REGISTERED = 'Não há unidades cadastrados no sistema. Cadastre uma nova entrada e tente novamente',
   NO_USUARIO_REGISTERED = 'Não há usuario cadastrados no sistema. Cadastre uma nova entrada e tente novamente',
   PACIENTE_NOT_FOUND = 'Nenhum paciente foi encontrado no sistema com as informações passadas. Verifique os dados e tente novamente',
+  UNIDADE_NOT_FOUND = 'Nenhuma unidade foi encontrada no sistema com as informações passadas. Verifique os dados e tente novamente',
   IMAGEM_NOT_FOUND = 'Nenhuma imagem foi encontrado no sistema com as informações passadas. Verifique os dados e tente novamente',
 
   // * Características de Pacientes
