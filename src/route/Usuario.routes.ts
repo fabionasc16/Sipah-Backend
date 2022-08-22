7cee9248ccdd201887d68c620ed5066e529d9fca
marcelopds70@gmail.com <marcelopds70@gmail.com>
2022-08-22 16:26:56 -0400

    Middlewares para validar acesso as rotas

--- src/route/Usuario.routes.ts
+++ src/route/Usuario.routes.ts
@@ -1,12 +1,14 @@
 import { UsuarioController } from 'controller/usuario.controller';
 import { Router } from 'express';
-
+import { AuthService } from 'service/auth.service';
+import { checkJWT } from '../middleware/checkJwt';
+import { checkRole } from '../middleware/checkRole';
 const usuarioRoutes = Router();
 
 const usuarioController = new UsuarioController();
 
 
-usuarioRoutes.post('/', usuarioController.createUsuario);
+usuarioRoutes.post('/',checkJWT, checkRole([AuthService.ROLES.USUARIO]), usuarioController.createUsuario);
 
 usuarioRoutes.get('/', usuarioController.listAllUsuario);
 
@@ -14,7 +16,7 @@ usuarioRoutes.get('/detalhes/:id', usuarioController.listUsuarioById);
 
 usuarioRoutes.get('/cpf/:cpf', usuarioController.listUsuarioByCPF);
 
-usuarioRoutes.delete('/:id', usuarioController.deleteUsuario);
+usuarioRoutes.delete('/:id',checkJWT, checkRole([AuthService.ROLES.USUARIO_EXCLUIR]), usuarioController.deleteUsuario);
 
 usuarioRoutes.put('/:id', usuarioController.updateUsuario);
 
