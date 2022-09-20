b8accf4b2becd89a58501ce58f04a7bd60545368
Marcelo Pinheiro <marcelopds70@gmail.com>
2022-09-20 10:11:21 -0400

    ajustes para o controle de acesso

--- src/route/Auth.routes.ts
+++ src/route/Auth.routes.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../service/auth.service';
 const authRoutes = Router();
 const authService = new AuthService();
 
-authRoutes.post('/', authService.authenticate);
+authRoutes.post('/auth', authService.authenticate);
 authRoutes.get('/unities', authService.unities)
 /*authRoutes.post('/forgotpassword', authService.forgotPass);
 authRoutes.post('/resetpassword', authService.resetPassword);
