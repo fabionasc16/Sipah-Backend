c16777b74665c8c33a9c902fe53329aa4414ec23
isaac.bandeira <isaac.bandeira@gmail.com>
2022-11-07 11:34:21 -0400

    correcao permite visualizar caracteristicas

--- src/route/Caracteristica.routes.ts
+++ src/route/Caracteristica.routes.ts
@@ -1,23 +1,62 @@
-import { CaracteristicaController } from '../controller/Caracteristica.Controller';
 import { Router } from 'express';
-import { AuthService } from '../service/auth.service';
+
+import { CaracteristicaController } from '../controller/Caracteristica.Controller';
 import { checkJWT } from '../middleware/checkJWT';
 import { checkRole } from '../middleware/checkRole';
+import { AuthService } from '../service/auth.service';
 
 const caracteristicaRoutes = Router();
 
 const caracteristicaController = new CaracteristicaController();
 
-caracteristicaRoutes.post('/', checkJWT, checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]), caracteristicaController.create);
+caracteristicaRoutes.post(
+  '/',
+  checkJWT,
+  checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]),
+  caracteristicaController.create,
+);
 
-caracteristicaRoutes.get('/', checkJWT, checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]), caracteristicaController.list);
+caracteristicaRoutes.get(
+  '/',
+  checkJWT,
+  checkRole([
+    AuthService.ROLES.ADMIN,
+    AuthService.ROLES.CARACTERISTICA,
+    AuthService.ROLES.PACIENTE_VISUALIZAR_FICHA_SOCIAL,
+  ]),
+  caracteristicaController.list,
+);
 
-caracteristicaRoutes.get('/:name', checkJWT, checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]), caracteristicaController.listByCaracteristica);
+caracteristicaRoutes.get(
+  '/:name',
+  checkJWT,
+  checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]),
+  caracteristicaController.listByCaracteristica,
+);
 
-caracteristicaRoutes.delete('/:id', checkJWT, checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]), caracteristicaController.delete);
+caracteristicaRoutes.delete(
+  '/:id',
+  checkJWT,
+  checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]),
+  caracteristicaController.delete,
+);
 
-caracteristicaRoutes.get('/id/:id', checkJWT, checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]), caracteristicaController.listById);
+caracteristicaRoutes.get(
+  '/id/:id',
+  checkJWT,
+  checkRole([
+    AuthService.ROLES.ADMIN,
+    AuthService.ROLES.CARACTERISTICA,
+    AuthService.ROLES.PACIENTE_VISUALIZAR_FICHA_SOCIAL,
+  ]),
+  caracteristicaController.listById,
+);
 
-caracteristicaRoutes.put('/:id', checkJWT, checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]), caracteristicaController.update);
+caracteristicaRoutes.put(
+  '/:id',
+  checkJWT,
+  checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.CARACTERISTICA]),
+  caracteristicaController.update,
+);
 
 export { caracteristicaRoutes };
