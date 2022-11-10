55d9bea76a9c9d6fc34d9e2210c4a46cf0b82c89
isaac.bandeira <isaac.bandeira@gmail.com>
2022-11-10 14:22:13 -0400

    listando interessado em ficha social do paciente

--- src/repository/InteressadoRepository.ts
+++ src/repository/InteressadoRepository.ts
@@ -1,13 +1,15 @@
-import { IInteressadoRepository } from '../repository/IInteressadoRepository';
-import { Interessado } from '../model/Interassado.model';
 import mongoose from 'mongoose';
 
-class InteressadoRepository implements IInteressadoRepository {
+import { Interessado } from '../model/Interassado.model';
+import { IInteressadoRepository } from './IInteressadoRepository';
 
-  //private usuario = usuario;
+class InteressadoRepository implements IInteressadoRepository {
+  // private usuario = usuario;
 
   async create(interessadoCadastro: any): Promise<any> {
-    interessadoCadastro.cpfSemFormatacao = interessadoCadastro.cpf.replaceAll('.', '').replaceAll('-', '');
+    interessadoCadastro.cpfSemFormatacao = interessadoCadastro.cpf
+      .replaceAll('.', '')
+      .replaceAll('-', '');
     const cadastroInteressado = await Interessado.create(interessadoCadastro);
 
     return cadastroInteressado;
@@ -15,7 +17,7 @@ class InteressadoRepository implements IInteressadoRepository {
 
   async listByCPF(cpf: string): Promise<any> {
     const data = await Interessado.findOne({
-      cpf
+      cpf,
     });
     return data;
   }
@@ -28,26 +30,48 @@ class InteressadoRepository implements IInteressadoRepository {
   }
 
   async listAllInteressado(params: any) {
-    let page = (params.currentPage != null ? (params.currentPage) + '' : '1');
-    let pageSize = params.perPage != null ? params.perPage : '10';
-    let search = params.search != null ? params.search : '';
+    const page = params.currentPage != null ? `${params.currentPage}` : '1';
+    const pageSize = params.perPage != null ? params.perPage : '10';
+    const search = params.search != null ? params.search : '';
     let filters = {};
 
     // Caso a uma palavra para busca seja enviada
     if (search) {
-      filters = { $and:[{ $or: [{ nome: search }, { cpf: search }, { cpfSemFormatacao: search }, { idPaciente: search }]}, { excluido:false }] };
+      filters = {
+        $and: [
+          {
+            $or: [
+              { nome: search },
+              { cpf: search },
+              { cpfSemFormatacao: search },
+              { idPaciente: search },
+            ],
+          },
+          { excluido: false },
+        ],
+      };
     }
 
-    let total = await Interessado.countDocuments(filters);
-    let pageNumber = await parseInt(page) - 1;
-    let pageSizeNumber = await parseInt(pageSize);
+    const total = await Interessado.countDocuments(filters);
+    const pageNumber = (await parseInt(page)) - 1;
+    const pageSizeNumber = await parseInt(pageSize);
 
-    let data = await Interessado.find(
+    const data = await Interessado.find(
       filters,
-      ' nome cpf  idPaciente ',
-      { skip: pageNumber * pageSizeNumber, limit: pageSizeNumber, sort:{ status:-1,nome:1} }).populate('idPaciente');
+      ' nome grauParentesco telefone cpf municipio created_at idPaciente ',
+      {
+        skip: pageNumber * pageSizeNumber,
+        limit: pageSizeNumber,
+        sort: { status: -1, nome: 1 },
+      },
+    ).populate('idPaciente');
 
-    let result = await { 'currentPage': page, 'perPage': pageSize, 'total': total, 'data': data };
+    const result = await {
+      currentPage: page,
+      perPage: pageSize,
+      total,
+      data,
+    };
 
     const data_a = await Interessado.find(filters, 'tipoCaracteristicas', {
       skip: pageNumber * pageSizeNumber,
@@ -57,31 +81,21 @@ class InteressadoRepository implements IInteressadoRepository {
     return result;
   }
 
-
-
   async delete(id: string): Promise<void> {
-    return await Interessado.findByIdAndUpdate(
-      { _id: id },
-      { excluido: true }
-    );
+    return await Interessado.findByIdAndUpdate({ _id: id }, { excluido: true });
   }
 
   async update(id: string, data: any): Promise<void> {
     data.updated_at = new Date();
-    await Interessado.findByIdAndUpdate(
-      { _id: id },
-      data
-     ,
-    );
+    await Interessado.findByIdAndUpdate({ _id: id }, data);
   }
 
   async adcionarIdPaciente(idPaciente: string, interessado: any): Promise<any> {
     interessado.idPaciente = idPaciente;
-    return await Interessado.create( interessado );
-
+    return await Interessado.create(interessado);
   }
 
-  /*async mudarStatus(id: string): Promise<void> {
+  /* async mudarStatus(id: string): Promise<void> {
     let interessadoEncontrado = await Interessado.findById({ _id: id });
     let status = !interessadoEncontrado.status;
     await Interessado.findByIdAndUpdate(
@@ -90,8 +104,7 @@ class InteressadoRepository implements IInteressadoRepository {
 
     );
     return await Interessado.findById({ _id: id });
-  }*/
-
+  } */
 }
 
 export { InteressadoRepository };
--- src/route/Interessado.routes.ts
+++ src/route/Interessado.routes.ts
@@ -1,29 +1,60 @@
-import { InteressadoController } from '../controller/Interessados.Controller';
 import { Router } from 'express';
-import { AuthService } from '../service/auth.service';
+
+import { InteressadoController } from '../controller/Interessados.Controller';
 import { checkJWT } from '../middleware/checkJWT';
 import { checkRole } from '../middleware/checkRole';
-
+import { AuthService } from '../service/auth.service';
 
 const interessadoRoutes = Router();
 
 const interessadoController = new InteressadoController();
 
+interessadoRoutes.post(
+  '/',
+  checkJWT,
+  checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]),
+  interessadoController.createInteressado,
+);
 
-interessadoRoutes.post('/',checkJWT, checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]), interessadoController.createInteressado);
+interessadoRoutes.get(
+  '/',
+  checkJWT,
+  checkRole([
+    AuthService.ROLES.ATENDIMENTO,
+    AuthService.ROLES.ADMIN,
+    AuthService.ROLES.PACIENTE_VISUALIZAR_FICHA_SOCIAL,
+  ]),
+  interessadoController.listAllInteressado,
+);
 
-interessadoRoutes.get('/',checkJWT, checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]), interessadoController.listAllInteressado);
+interessadoRoutes.get(
+  '/detalhes/:id',
+  checkJWT,
+  checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]),
+  interessadoController.listInteressadoById,
+);
 
-interessadoRoutes.get('/detalhes/:id',checkJWT, checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]), interessadoController.listInteressadoById);
+interessadoRoutes.get(
+  '/cpf/:cpf',
+  checkJWT,
+  checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]),
+  interessadoController.listInteressadoByCPF,
+);
 
-interessadoRoutes.get('/cpf/:cpf',checkJWT, checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]), interessadoController.listInteressadoByCPF);
+interessadoRoutes.delete(
+  '/:id',
+  checkJWT,
+  checkRole([AuthService.ROLES.ADMIN, AuthService.ROLES.ATENDIMENTO]),
+  interessadoController.deleteInteressado,
+);
 
-interessadoRoutes.delete('/:id',checkJWT, checkRole([ AuthService.ROLES.ADMIN, AuthService.ROLES.ATENDIMENTO]), interessadoController.deleteInteressado);
+interessadoRoutes.put(
+  '/:id',
+  checkJWT,
+  checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]),
+  interessadoController.updateInteressado,
+);
 
-interessadoRoutes.put('/:id', checkJWT, checkRole([AuthService.ROLES.ATENDIMENTO, AuthService.ROLES.ADMIN]), interessadoController.updateInteressado);
-
-
-
-//interessadoRoutes.put('/mudar/status/:id', interessadoController.mudarStatus);
+// interessadoRoutes.put('/mudar/status/:id', interessadoController.mudarStatus);
 
 export { interessadoRoutes };
